da516203c6379f0606e05f461cc156efd37be522
richmass1 <103288353+richmass1@users.noreply.github.com>
2024-05-22 17:05:39 -0700

    Add files via upload

---
+++ sum[1].js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {Number} a - first operand
+ * @param {Number} b - second operand
+ * @returns the sum
+ */
+function sum (a, b) {
+  return a + b
+}
+/**
+ * Test
+ */
+function test () {
+
+}
+
+export { sum, test }
